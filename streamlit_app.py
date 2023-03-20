a1669437d3c3b017d80699143251e8b65d2b3a86
alalei2023 <128339380+alalei2023@users.noreply.github.com>
2023-03-19 21:09:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit
 
 streamlit.title('My parents NEw Healthy Diner')
-streamlit.headr('Breakfast menu')
-
-
-
-
+streamlit.header('Breakfast menu')
 streamlit.text('Fruits & sunny egg')
 streamlit.text('corns')
